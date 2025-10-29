4a5f508ad573c3dfc2d8b7a66a9238ca84f07376
Prohalladdas <prolladdas@gmail.com>
2025-10-29 20:32:06 +0530

    Rendering a Users Booked Tours

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -2,6 +2,7 @@ const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const Booking = require('../models/bookingModel');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data form collection
@@ -43,6 +44,20 @@ exports.getAccount = (req, res) => {
     title: 'Your account',
   });
 };
+
+exports.getMyTours = catchAsync(async (req, res, next) => {
+  // Find all my bookings
+  const bookings = await Booking.find({ user: req.user.id });
+
+  // Find tours with the returned IDs
+  const tourIds = bookings.map((el) => el.tour);
+  const tours = await Tour.find({ _id: { $in: tourIds } });
+
+  res.status(200).render('overview', {
+    title: 'My Tours',
+    tours,
+  });
+});
 
 exports.updataUserData = catchAsync(async (req, res, next) => {
   const updatedUser = await User.findByIdAndUpdate(
