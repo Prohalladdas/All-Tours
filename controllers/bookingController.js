f9c6a94f324f06f2e1d5c8037d96fb64cbe3f1d0
Prohalladdas <prolladdas@gmail.com>
2025-11-02 19:31:54 +0530

    Fixed bug 1

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -82,7 +82,7 @@ exports.webhookChekout = (req, res, next) => {
 };
 
 exports.createBooking = factory.createOne(Booking);
-exports.geteBooking = factory.getOne(Booking);
+exports.getBooking = factory.getOne(Booking);
 exports.getAllBookings = factory.getAll(Booking);
 exports.updateBooking = factory.updateOne(Booking);
 exports.deleteBooking = factory.deleteOne(Booking);
