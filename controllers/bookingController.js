8ae6efda080b910553d5f378f447f1b3f46f71de
Prohalladdas <prolladdas@gmail.com>
2025-11-02 19:45:43 +0530

    Fixed bug 3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -104,7 +104,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     mode: 'payment',
-    // success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
     success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
